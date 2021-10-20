3e81b52255f06a64652ddc4ad0023559ee3cea78
Zeyang Lai <decyctus@gmail.com>
2021-10-19 19:03:54 -0700

    finished writing the function to render different type of member

--- index.js
+++ index.js
@@ -5,9 +5,8 @@ const Manager = require("./lib/Manager");
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-let manager={};
-const engineer =[];
-const intern =[];
+const listing =[];
+
 
 function getManager(){
     return inquirer.prompt([
@@ -38,14 +37,14 @@ function getManager(){
             choices:["Engineer","Intern","I don't want to add any more team members"],
         },
     ]).then((data)=>{
-        manager=new Manager(data.name,data.id,data.email,data.office);
+        listing.push(new Manager(data.name,data.id,data.email,data.office));
         switch(data.member){
             case "Engineer":
                 return addEngineer();
             case "Intern":
                 return addIntern();
             default:
-                return renderListing();
+                return renderHtml();
         }
     });
 }
@@ -79,14 +78,14 @@ function addEngineer(){
             choices:["Engineer","Intern","I don't want to add any more team members"],
         },
     ]).then((data)=>{
-        engineer.push(new Engineer(data.name,data.id,data.email,data.github));
+        listing.push(new Engineer(data.name,data.id,data.email,data.github));
         switch(data.member){
             case "Engineer":
                 return addEngineer();
             case "Intern":
                 return addIntern();
             default:
-                return renderListing();
+                return renderHtml();
         }
     });
 }
@@ -120,22 +119,89 @@ function addIntern(){
             choices:["Engineer","Intern","I don't want to add any more team members"],
         },
     ]).then((data)=>{
-        intern.push(new Intern(data.name,data.id,data.email,data.school));
+        listing.push(new Intern(data.name,data.id,data.email,data.school));
         switch(data.member){
             case "Engineer":
                 return addEngineer();
             case "Intern":
                 return addIntern();
             default:
-                return renderListing();
+                return renderHtml();
         }
     });
 }
 
-function renderListing(){
-    console.log(manager)
-    console.log(engineer)
-    console.log(intern)
+function renderManager(manager){
+    return`
+    <div class="col-4 mt-4">
+      <div class="card h-100">
+        <div class="card-header">
+            <h3>${manager.name}</h3>
+            <h4>Manager</h4><i class="material-icons">content_paste</i>
+        </div>
+        <div class="card-body">
+            <p class="id">ID: ${manager.id}</p>
+            <p class="email">Email: <a href="#">${manager.email}</a></p>
+            <p class="office">Office Number: ${manager.officeNumber}</p>
+        </div>
+      </div>
+    </div>
+    `
+}
+
+function renderEngineer(engineer){
+    return`
+    <div class="col-4 mt-4">
+      <div class="card h-100">
+        <div class="card-header">
+            <h3>${engineer.name}</h3>
+            <h4>Engineer</h4><i class="material-icons">laptop_mac</i>
+        </div>
+        <div class="card-body">
+            <p class="id">ID: ${engineer.id}</p>
+            <p class="email">Email: <a href="#">${engineer.email}</a></p>
+            <p class="github">Github: <a href="#">${engineer.github}</a></p>
+        </div>
+      </div>
+    </div>
+    `
+}
+
+
+function renderIntern(intern){
+    return`
+    <div class="col-4 mt-4">
+      <div class="card h-100">
+        <div class="card-header">
+            <h3>${intern.name}</h3>
+            <h4>Intern</h4><i class="material-icons">assignment_ind</i>
+        </div>
+        <div class="card-body">
+            <p class="id">ID: ${intern.id}</p>
+            <p class="email">Email:<a href="#}">${intern.email}</a></p>
+            <p class="school">School: ${intern.school}</p>
+        </div>
+      </div>
+    </div>
+    `
+}
+
+function renderCard(member){
+    switch(member.getRole()){
+        case "Manager":
+            return renderManager(member);
+        case "Engineer":
+            return renderEngineer(member);
+        case "Intern":
+            return renderIntern(member);
+        default:
+            return "";
+    }
+}
+
+
+function renderHtml(){
+    console.log(listing)
 }
 
 getManager();
